73c93879182e93a861cafcdf2f5d4802d9e5728a
mebegu <mehmetgurcay@ku.edu.tr>
2016-11-12 18:56:26 +0300

    profile pic func

--- controllers/others/fileController.js
+++ controllers/others/fileController.js
@@ -1,5 +1,6 @@
 
 const Index = require('./../../index');
+const User = require('./../../models/User');
 const busboyBodyParser = require('busboy-body-parser');
 const fs = require('fs');
 
@@ -7,26 +8,80 @@ const fs = require('fs');
 exports.uploadFile = function(req,res,next){
 
   console.log("Upload request recieved.");
-  console.log(req.file);
+  console.log(req.body);
+  if(!req.user._id){
+    console.log("success: false, details: Autherization failed.");
+    return res.status(401).send({"success":false, "detail": "Autherization failed!"});
+  }
+  var operation = req.body.operation;
 
-  var filename = req.file.filename;
-  var path = req.file.path;
-  var type = req.file.mimetype;
+  if(!operation){
+    console.log("success: false, details: operation was not set!");
+    return res.status(200).send({"success":false, "detail": "operation was not set!"});
+  }
 
-  var read_stream =  fs.createReadStream(path);
+  if(operation != 1){
+    console.log("success: false, details: operation was not set!");
+    return res.status(200).send({"success":false, "detail": "operation was not set!"});
+  }
+
+  switch(operation) {
+    case '1':
+      var filename = req.file.filename;
+      var path = req.file.path;
+      var type = req.file.mimetype;
+      var ownerid = req.user._id;
+      var oldimg;
+      console.log("operation 1 started");
+
+      User.find({_id: ownerid}, {photo: 1}, function (err, docs) {
+        if(err){
+          console.log("Internal db error");
+          console.log(err);
+          return res.status(500).send({"success":false, "details": "Internal DB error, check query!", "error": err});
+        }
+
+        oldimg = docs.photo;
+        console.log("oldimg"+ oldimg+ " "+docs.photo);
+
+      });
+
+      var read_stream =  fs.createReadStream(path);
+      var writeStream = gfs.createWriteStream({
+        filename: filename,
+        ownerid: ownerid
+      });
+
+      read_stream.pipe(writeStream);
+
+      writeStream.on('close', function(file) {
+        newimg = file._id;
+        console.log("newimg"+ newimg+ " "+file._id);
+        if(oldimg){
+          console.log("triying to remove "+oldimg);
+          gfs.remove({_id: oldimg}, function(err){
+            if(err){
+              return console.log("couldnt delete");
+
+            console.log("removed");
+            }
+          });
+        }
+        console.log("ownerid "+ownerid);
+        User.update({_id: ownerid}, {$set: {photo: newimg}});
+        writeStream.end();
+        fs.unlink(path);
+        return res.status(200).send({"success":true, "detail": "Profile Photo is changed!"});
+      });
+
+      break;
+    default:
+      console.log("success: false, details: Unknown Operation!");
+      return res.status(200).send({"success":false, "detail": "Unknown Operation!"});
+      break;
+  }
 
 
-  var writeStream = gfs.createWriteStream({
-    filename: filename
-  });
-
-  read_stream.pipe(writeStream);
-
-  writeStream.on('close', function(file) {
-    console.log(file.filename);
-    writeStream.end();
-    return res.status(200).send({success: true});
-  });
 
 };
 
