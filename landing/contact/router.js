d1ee5398655537b6a6c9a99250db82f4ce7722af
mebegu <mehmetgurcay@ku.edu.tr>
2017-07-23 22:55:04 +0300

    landing page contact form integration with slack

--- landing/contact/router.js
+++ landing/contact/router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const query = require('./../../product/utility/query');
 const model = require('./model');
+const request = require('superagent');
+const winston = require('winston');
+const config = require('./../../product/config');
 
 const param = function(req,res,next){
-    console.log('contact request received');
     req.args = {model: model,
             getSelect: {},
             listSelect:{email: 1, type: 1, date: 1, country:1},
@@ -12,10 +14,27 @@ const param = function(req,res,next){
     next();
 }
 
+const sendSlack = function(req,res,next){
+    let message = "From:\n*"+req.body.name+  " " +req.body.surname+ "* `<" +req.body.email+ ">`\n" 
+                  +req.body.type+ " - " +req.body.institution+ " (" +req.body.country+ ")\n"+
+                  ">"+req.body.message+ "\n\n\n";
+    
+        request
+          .post(config.slackContactWebHook)
+          .send({"text": message, username: "lc.bot", icon_emoji: ":lc_logo:", mrkdwn: true})
+          .end(function(err,resp){
+            if(err)
+                winston.log('error', 'Slack WebHook Error', {error: err});
+            else
+                winston.log('debug', 'Contact message sent to slack');
+          });
+    next();
+}
+
 module.exports = function (app) {
     const routes = express.Router();
 
-    routes.post('/create',  param,  query.create);
+    routes.post('/create',  param, sendSlack,  query.create);
     routes.post('/edit',    param,  query.edit);
     routes.get('/:id',      param,  query.get);
     routes.get('/',         param,  query.list);
